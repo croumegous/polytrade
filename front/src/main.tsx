38142561e3ac2f9625baa96685e7d7c93824f588
leofvo <leofvo@pm.me>
2022-02-19 18:08:18 +0100

    build: react + typescript + mui template

---
+++ front/src/main.tsx
@@ -0,0 +1,12 @@
+import "./main.css";
+
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import App  from "./pages/App";
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
